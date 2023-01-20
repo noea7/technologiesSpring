bd06486511e314c68c1e39da568c4aa75955243d
noea7 <114760692+noea7@users.noreply.github.com>
2023-01-20 09:17:21 +0200

    initial commit

---
+++ blogging-api/src/test/java/lt/techin/kristina/bloggingapi/BloggingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.kristina.bloggingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
